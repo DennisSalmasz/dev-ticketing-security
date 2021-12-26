bf18c5205867b67882ac8060b55596de9d55c1e2
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-26 00:01:23 +0000

    security - remember me

--- src/main/java/com/cyber/config/SecurityConfig.java
+++ src/main/java/com/cyber/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package com.cyber.config;
 
+import com.cyber.service.SecurityService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.security.Provider;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private SecurityService securityService;
+
+    public SecurityConfig(SecurityService securityService) {
+        this.securityService = securityService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,6 +40,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/login");
+                    .logoutSuccessUrl("/login")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds(120)
+                    .key("ticketNGSecret")
+                    .userDetailsService(securityService);
+
     }
 }
