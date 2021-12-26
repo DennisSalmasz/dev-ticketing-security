03c5376b52645fb6143c80f6776e03b5b9ae2589
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-26 22:34:54 +0000

    bug fix

--- src/main/java/com/cyber/entity/BaseEntityListener.java
+++ src/main/java/com/cyber/entity/BaseEntityListener.java
@@ -13,36 +13,35 @@ import java.time.LocalDateTime;
 @Component
 public class BaseEntityListener extends AuditingEntityListener {
 
-    //security will tell us who did what, when... through AuditingEntityListener class
-
-    //Persist - save data in DB
-    //PrePersist - this method will run before saving each data in DB
     @PrePersist
     private void onPrePersist(BaseEntity baseEntity){
 
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        baseEntity.insertDateTime = LocalDateTime.now();
-        baseEntity.lastUpdateDateTime=LocalDateTime.now();
-        baseEntity.insertUserId=1L;
-        baseEntity.lastUpdateUserId=1L;
 
-        if(authentication != null && !authentication.getName().equals("anonymousUser")){
+        baseEntity.insertDateTime = LocalDateTime.now();
+        baseEntity.lastUpdateDateTime = LocalDateTime.now();
+        baseEntity.insertUserId = 1L;
+        baseEntity.lastUpdateUserId = 1L;
+
+        if (authentication != null && !authentication.getName().equals("anonymousUser")){
             Object principal = authentication.getPrincipal();
-            baseEntity.insertUserId = ((UserPrincipal) principal).getId();
-            baseEntity.lastUpdateUserId = ((UserPrincipal) principal).getId();
+            baseEntity.insertUserId=((UserPrincipal) principal).getId();
+            baseEntity.lastUpdateUserId=((UserPrincipal) principal).getId();
         }
     }
 
-    //this will run before each data update in DB
+
     @PreUpdate
     private void onPreUpdate(BaseEntity baseEntity){
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        baseEntity.lastUpdateDateTime=LocalDateTime.now();
-        baseEntity.lastUpdateUserId=1L;
 
-        if(authentication != null && !authentication.getName().equals("anonymousUser")){
+        baseEntity.lastUpdateDateTime = LocalDateTime.now();
+        baseEntity.lastUpdateUserId = 1L;
+
+        if (authentication != null && !authentication.getName().equals("anonymousUser")){
             Object principal = authentication.getPrincipal();
-            baseEntity.lastUpdateUserId = ((UserPrincipal) principal).getId();
+
+            baseEntity.lastUpdateUserId=((UserPrincipal) principal).getId();
         }
     }
 }
